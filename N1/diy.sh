863532146219b90b1648cc582b3eb49fe4cdeab3
Jason9612 <38736971+Jason9612@users.noreply.github.com>
2023-09-07 19:22:03 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,7 +4,6 @@
 sed -i 's/192.168.1.1/192.168.1.211/g' package/base-files/files/bin/config_generate
 
 # Firewall
-echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
