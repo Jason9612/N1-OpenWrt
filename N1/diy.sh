c99d031e18280ae34325e99089c6d4fa04922bf1
Jason9612 <38736971+Jason9612@users.noreply.github.com>
2023-09-12 01:30:34 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,11 +6,11 @@ sed -i 's/192.168.1.1/192.168.1.211/g' package/base-files/files/bin/config_gener
 # Firewall
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/haproxy
 rm -rf feeds/packages/net/v2ray-geodata
 
 # Add packages
 svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
-svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
+
